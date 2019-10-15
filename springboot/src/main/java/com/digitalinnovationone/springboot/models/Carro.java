59f9faeeb5132013e0c7f5e65c04bb02c857c0f5
William Aparecido Campos <william_apr16@hotmail.com>
2019-10-15 10:40:26 -0300

    Post e Delete Carro

--- springboot/src/main/java/com/digitalinnovationone/springboot/models/Carro.java
+++ springboot/src/main/java/com/digitalinnovationone/springboot/models/Carro.java
@@ -26,7 +26,35 @@ public class Carro implements Serializable {
     @NotNull
     private String placa;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
 
+    public String getMarca() {
+        return marca;
+    }
 
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
 }
--- springboot/src/main/java/com/digitalinnovationone/springboot/resources/CarroResource.java
+++ springboot/src/main/java/com/digitalinnovationone/springboot/resources/CarroResource.java
@@ -3,12 +3,12 @@ package com.digitalinnovationone.springboot.resources;
 import com.digitalinnovationone.springboot.models.Carro;
 import com.digitalinnovationone.springboot.repository.CarroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/api")
+@RestController
+@RequestMapping(value = "/api")
 public class CarroResource {
     @Autowired
     CarroRepository carroRepository;
@@ -17,5 +17,23 @@ public class CarroResource {
     public List<Carro> listaCarros(){
         return carroRepository.findAll();
     }
+
+    @PostMapping("/carro")
+    public Carro salvarCarro(@RequestBody Carro carro){
+        return carroRepository.save(carro);
+    }
+
+    @PutMapping("/carro")
+    public  Carro atualizarCarro(@RequestBody Carro carro){
+        return carroRepository.save(carro);
+    }
+
+    @DeleteMapping("/carro")
+    public void deletarCarro(@RequestBody Carro carro){
+        carroRepository.delete(carro);
+    }
+
+
+
 
 }
